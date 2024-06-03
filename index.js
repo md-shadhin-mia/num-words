bb3fe72ec94704718515c6b346b3703a95b2f1bf
MD Shadhin <65449833+md-shadhin-mia@users.noreply.github.com>
2024-06-03 14:51:49 +0600

    add floating converting function

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 /* eslint-disable eqeqeq */
 
-const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen ']
-const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-const regex = /^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/
+const regex = /^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/;
 
-const getLT20 = (n) => a[Number(n)]
-const getGT20 = (n) => b[n[0]] + ' ' + a[n[1]]
+const getLT20 = (n) => a[Number(n)];
+const getGT20 = (n) => b[n[0]] + ' ' + a[n[1]];
 
-module.exports = function numWords (input) {
-  const num = Number(input)
-  if (isNaN(num)) return ''
-  if (num === 0) return 'zero'
-
-  const numStr = num.toString()
+const convertIntegerPart = (numStr) => {
   if (numStr.length > 9) {
-    throw new Error('overflow') // Does not support converting more than 9 digits yet
+    throw new Error('overflow'); // Does not support converting more than 9 digits yet
   }
 
-  const [, n1, n2, n3, n4, n5] = ('000000000' + numStr).substr(-9).match(regex) // left pad zeros
+  const [, n1, n2, n3, n4, n5] = ('000000000' + numStr).substr(-9).match(regex); // left pad zeros
 
-  let str = ''
-  str += n1 != 0 ? (getLT20(n1) || getGT20(n1)) + 'crore ' : ''
-  str += n2 != 0 ? (getLT20(n2) || getGT20(n2)) + 'lakh ' : ''
-  str += n3 != 0 ? (getLT20(n3) || getGT20(n3)) + 'thousand ' : ''
-  str += n4 != 0 ? getLT20(n4) + 'hundred ' : ''
-  str += n5 != 0 && str != '' ? 'and ' : ''
-  str += n5 != 0 ? (getLT20(n5) || getGT20(n5)) : ''
+  let str = '';
+  str += n1 != 0 ? (getLT20(n1) || getGT20(n1)) + 'crore ' : '';
+  str += n2 != 0 ? (getLT20(n2) || getGT20(n2)) + 'lakh ' : '';
+  str += n3 != 0 ? (getLT20(n3) || getGT20(n3)) + 'thousand ' : '';
+  str += n4 != 0 ? getLT20(n4) + 'hundred ' : '';
+  str += n5 != 0 && str != '' ? 'and ' : '';
+  str += n5 != 0 ? (getLT20(n5) || getGT20(n5)) : '';
 
-  return str.trim()
+  return str.trim();
 }
+
+const convertFractionalPart = (fractionStr) => {
+  let str = 'point ';
+  for (const digit of fractionStr) {
+    str += getLT20(digit) + ' ';
+  }
+  return str.trim();
+}
+
+module.exports = function numWords(input) {
+  const num = Number(input);
+  if (isNaN(num)) return '';
+  if (num === 0) return 'zero';
+
+  const [integerPart, fractionalPart] = input.toString().split('.');
+
+  let result = convertIntegerPart(integerPart);
+
+  if (fractionalPart) {
+    result += ' ' + convertFractionalPart(fractionalPart);
+  }
+
+  return result.trim();
+}
+
